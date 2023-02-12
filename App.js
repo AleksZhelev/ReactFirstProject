23066d3a36eb8b73aa4f1c4eecc3d078f8b8b3d7
Aleksandar Zhelev <73350032+AleksZhelev@users.noreply.github.com>
2023-02-12 15:52:32 +0200

    Added DarkMode functionality

--- App.js
+++ App.js
@@ -3,10 +3,22 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+    
+    const [darkMode, setDarkMode] = React.useState(false)
+    
+    function toggleDarkMode(){
+        setDarkMode(prevState => !prevState)
+    }
+    
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                toggleDarkMode={toggleDarkMode}
+                darkMode={darkMode}
+            />
+            <Main 
+                darkMode={darkMode}
+            />
         </div>
     )
 }
